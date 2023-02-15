235694b2752764d68ab017431fc1bc688830a80e
mosesochacha <ochachamoses443@@gmail.com>
2023-02-15 15:52:35 +0300

    done with refactoring and solving

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,13 @@
 require 'pry'
-
+require_relative '../lib/concerns/memorable'
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+    prepend Parable
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -17,20 +16,9 @@ class Song
   def self.all
     @@songs
   end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+
 end
